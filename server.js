d792d8d8c5d2f53221387961d1c3f56694f283a4
adhammhaydat <adhammohidat123@gmail.com>
2021-08-30 15:56:20 +0300

    new repo

--- server.js
+++ server.js
@@ -1,85 +1,87 @@
-'use strict'
-
-require('dotenv').config();
-const express = require('express')
-const cors = require('cors');
-const mongoose = require("mongoose");
+"use strict";
+const util = require("util");
+const TextEncoder = new util.TextEncoder();
+const express = require("express");
+const cors = require("cors");
 const axios = require("axios");
-
+const dotenv = require("dotenv").config();
 const server = express();
-server.use(cors());
 server.use(express.json());
-
+server.use(cors());
+const mongoose = require("mongoose");
 const PORT = process.env.PORT;
-// const PORT=3010;
 
-mongoose.connect(`mongodb://localhost:27017/drinks`, { useNewUrlParser: true });
+mongoose.connect(
+  "mongodb://adhammhaydat:12345@cluster0-shard-00-00.qi4a6.mongodb.net:27017,cluster0-shard-00-01.qi4a6.mongodb.net:27017,cluster0-shard-00-02.qi4a6.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-ipru7s-shard-0&authSource=admin&retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
-const drink = new mongoose.Schema ({
-    strDrink: String,
-    strDrinkThumb: String,
-    idDrink: String
-})
+const flowersSchema = new mongoose.Schema({
+  instructions: String,
+  photo: String,
+  name: String,
+});
 
-const drinkModel= mongoose.model('drinks',drink);
-
-
-server.get(`/getdrinks`,getDrinks);
-server.post(`addtofavorite`,addToFavorite);
-server.get(`getfavorite`,getFavorite);
-server.delete(`/delete/:id`,deleteDri);
-server.put(`/update/:id`,updateDri);
-
-function getDrinks (req,res) {
-    axios.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic`).then(result => {
-        res.send(result).data;
-    }
+const flowerModel = mongoose.model("flowers", flowersSchema);
+server.get("/", (req, res) => {
+  res.send("hello");
+});
+function showAllData(req, res) {
+  axios
+    .get(
+      `https://flowers-api-13.herokuapp.com/getFlowers
+  `
     )
+    .then((result) => {
+      res.send(result.data);
+    });
+}
+function addToFav(req, res) {
+  const { instructions, photo, name } = req.body;
+  const newFlowers = new flowerModel({
+    instructions: instructions,
+    photo: photo,
+    name: name,
+  });
+  newFlowers.save();
+  res.send(newFlowers);
+}
+function showFav(req, res) {
+  flowerModel.find({}, (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  });
 }
 
-function addToFavorite (req,res) {
-    const {strDrink,strDrinkThumb,idDrink} = req.body;
-    const newDrink = new drinkModel ({
-        strDrink: strDrink,
-        strDrinkThumb: strDrinkThumb,
-        idDrink: idDrink
-    })
-    newDrink.save();
+function deleteFav(req, res) {
+  let id = req.params.id;
+  flowerModel.findByIdAndDelete(id, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
 }
-
-function getFavorite (req,res) {
-    drinkModel.find({},(err,data) => {res.send(data);
-    })
+function updatData(req, res) {
+  let id = req.params.id;
+  const { instructions, photo, name } = req.body;
+  flowerModel.findOne({ _id: id }, (err, result) => {
+    result.instructions = instructions;
+    result.photo = photo;
+    result.name = name;
+    result.save();
+    res.send(result);
+  });
 }
-
-function deleteDri (req,res) {
-    const id = req.params.id;
-    drinkModel.findByIdAndDelete(id,(err,data) => {
-        drinkModel.find ({},(arr,newData) => {
-            res.send(newData)
-        })
-    })
-}
-
-function updateDri (req,res) {
-    drinkModel.find({},(err,data) => {
-        data.map((item,idx) => {
-            if (idx == req.params.id){
-                item.strDrink=req.body.strDrink;
-                item.strDrinkThumb=req.body.strDrinkThumb;
-                item.idDrink=req.body.idDrink;
-                item.save()
-            }
-        })
-        res.send(data);
-    })
-}
-
-server.get('/',
- function (req, res) {
-  res.send('Hello World')
-})
-
-
-server.listen(PORT, () => console.log(`listening on ${PORT}`))
-// server.listen(3010,() => {console.log('listening on 3010');
+server.get("/showData", showAllData);
+server.post("/addfav", addToFav);
+server.get("/showFav", showFav);
+server.delete("/deleteFav/:id", deleteFav);
+server.put("/updatData/:id", updatData);
+server.listen(PORT, () => {
+  console.log(`i am a live on port ${PORT}`);
+});
